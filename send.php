fb0da2ec88fb0680e8b35581a33432d477c0d8ce
rud3nk0 <130237226+rud3nk0@users.noreply.github.com>
2024-04-12 00:04:44 +0300

    clone

---
+++ send.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+    $to = 'Redhat_1@mail.ru'; 
+    $subject ='Сообщение с сайта RedHat '; 
+    $message = '
+            <html>
+                <head>
+                    <title>'.$subject.'</title>
+                </head>
+                <body>
+                    <p>Заголовок: '.$_POST['title'].'</p>
+                    <p>Имя: '.$_POST['name'].'</p>
+                    <p>Телефон: '.$_POST['tel'].'</p>
+                </body>
+            </html>'; 
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+    $headers .= "From: RedHat <kalinovsdky.job@example.com>\r\n"; 
+    mail($to, $subject, $message, $headers); 
+
+?>
